50d266b2b3b78fd10fba401b41d37188ef135ce3
Fikrii21 <101777519+Fikrii21@users.noreply.github.com>
2022-04-08 02:55:54 +0700

    Update config.js
    .

--- config.js
+++ config.js
@@ -3,12 +3,12 @@ const fs = require('fs')
 const chalk = require('chalk')
 
 
-global.owner = ['6281220670449','0']
+global.owner = ['6281233738677','0']
 global.packname = 'Bot Whatsapp'
 global.author = 'Nezuko'
 global.sessionName = 'data'
 global.prefa = ['','!','.','#']
-global.ownername = 'KurrXd'
+global.ownername = 'Fikrii OFC'
 global.sp = '>'
 global.mess = {
     admin: 'Fitur Khusus Admin Group!',
